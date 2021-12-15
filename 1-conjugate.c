d3f2e29b788ab879f113272296df062d1d22fcbb
threadsv1 <majdimed3@gmail.com>
2021-12-15 18:30:06 +0100

    edited

---
+++ 1-conjugate.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * conjugate - return the conjugate of a given complex number
+ *@c: structure of a complex number
+ * Return: c
+ */
+complex conjugate(complex c)
+{
+c.im *= -1;
+return (c);
+}
